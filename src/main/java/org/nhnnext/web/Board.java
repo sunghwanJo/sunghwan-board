26cecc5a59f6839b707c4d14bba5268716db6208
sunghwan <sunghwanjo@nhnnext.org>
2013-10-02 17:00:21 +0900

    * Controller에서 Database에 연결할 수 있게 @Autowired annotation추가

--- src/main/java/org/nhnnext/web/Board.java
+++ src/main/java/org/nhnnext/web/Board.java
@@ -21,6 +21,38 @@ public class Board {
 	
 	@Column(length=100, nullable=false)
 	private String fileName;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
 	
 	
 	
--- src/main/java/org/nhnnext/web/BoardController.java
+++ src/main/java/org/nhnnext/web/BoardController.java
@@ -1,5 +1,7 @@
 package org.nhnnext.web;
 
+import org.nhnnext.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value="/board")
 public class BoardController {
+	@Autowired
+	private BoardRepository boardRepository;
+	
 	
 	@RequestMapping(value="/write")
 	public String write(){
