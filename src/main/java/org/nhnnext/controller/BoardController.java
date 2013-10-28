920efb88126b26061e9e7cf6b4944a5bcb2866ed
sunghwan <sunghwanjo@nhnnext.org>
2013-10-28 22:37:58 +0900

    try -catch 문을 추가하다 문득 깨달았다. 내 소스에서 어떤 Exception을 Throws해야 하는지도 모르는데 무의미한 catch문을 만드는게 의미가 있을까...

--- src/main/java/org/nhnnext/controller/BoardController.java
+++ src/main/java/org/nhnnext/controller/BoardController.java
@@ -18,108 +18,133 @@ import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Board 관련해 게시글 올리기, 수정, 삭제, 보여주기의 역할을 하는 Controller
+ * 
  * @author josunghwan
- *
+ * 
  */
 
 @Controller
-@RequestMapping(value="/board")
+@RequestMapping(value = "/board")
 public class BoardController {
-	@Autowired	
+	@Autowired
 	private BoardRepository boardRepository;
-	
+
 	@Autowired
 	private CommentRepository commentRepository;
-	
+
 	/**
 	 * id값을 기반으로 게시글을 Database에서 삭제함
-	 * @param id Board의 id
+	 * 
+	 * @param id
+	 *            Board의 id
 	 * @return redirect:/
 	 */
-	@RequestMapping(value="/delete/{id}")
-	public String delete(@PathVariable Long id){
-		Board board = boardRepository.findOne(id);
-		for(Comment c : board.getComments()){
-			commentRepository.delete(c);
+	@RequestMapping(value = "/delete/{id}")
+	public String delete(@PathVariable Long id) {
+
+		try {
+			Board board = boardRepository.findOne(id);
+			for (Comment c : board.getComments()) {
+				commentRepository.delete(c);
+			}
+			boardRepository.delete(id);
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		boardRepository.delete(id);
-		
+
 		return "redirect:/";
 	}
-	
+
 	/**
 	 * 글쓰는 화면을 보여줌
+	 * 
 	 * @return form.jsp
 	 */
-	@RequestMapping(value="/write")
-	public String write(){
+	@RequestMapping(value = "/write")
+	public String write() {
 		return "list";
 	}
-	
+
 	/**
 	 * 
 	 * @param id
-	 * @param model Model임
+	 * @param model
+	 *            Model임
 	 * @return update.jsp
 	 */
-	@RequestMapping(value="/update/{id}")
-	public String update(@PathVariable Long id, Model model){
+	@RequestMapping(value = "/update/{id}")
+	public String update(@PathVariable Long id, Model model) {
 		Board getBoard = boardRepository.findOne(id);
 		model.addAttribute("board", getBoard);
-		
+
 		return "update";
 	}
-	
+
 	/**
-	 * @param board Form정보를 기반으로한 board 인스턴
-	 * @param file 이미지파일
+	 * @param board
+	 *            Form정보를 기반으로한 board 인스턴
+	 * @param file
+	 *            이미지파일
 	 * @return show.jsp
 	 */
-	@RequestMapping(value="", method=RequestMethod.POST)
-	public String create(Board board, MultipartFile file){
-		System.out.println("Board" + board);
-		
-		FileUploader.upload(file);
-		board.setFileName(file.getOriginalFilename());
-		Board savedBoard = boardRepository.save(board);
-	
+	@RequestMapping(value = "", method = RequestMethod.POST)
+	public String create(Board board, MultipartFile file) {
+		try {
+			System.out.println("Board" + board);
+
+			FileUploader.upload(file);
+			board.setFileName(file.getOriginalFilename());
+			Board savedBoard = boardRepository.save(board);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return "redirect:/board/list";
 	}
 
 	/**
 	 * list를 보여준다.
-	 * @param model Model임..
+	 * 
+	 * @param model
+	 *            Model임..
 	 * @return list.jsp
 	 */
-	@RequestMapping(value="/list")
+	@RequestMapping(value = "/list")
 	public String list(Model model) {
+		try{
 		Iterable<Board> boards = boardRepository.findAll();
-		
-		Collections.reverse( (List<Board>) boards );
+
+		Collections.reverse((List<Board>) boards);
 		model.addAttribute("boards", boards);
-		
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return "list";
 	}
-	
+
 	/**
 	 * 
-	 * @param board id를 기반으로 가져온 board instance
-	 * @param file	이미지파일
-	 * @param id	수정하려는 Board의 id
+	 * @param board
+	 *            id를 기반으로 가져온 board instance
+	 * @param file
+	 *            이미지파일
+	 * @param id
+	 *            수정하려는 Board의 id
 	 * @return
 	 */
-	@RequestMapping(value="{id}", method=RequestMethod.POST)
-	public String update(Board board, MultipartFile file, @PathVariable Long id){
-		
+	@RequestMapping(value = "{id}", method = RequestMethod.POST)
+	public String update(Board board, MultipartFile file, @PathVariable Long id) {
+
 		FileUploader.upload(file);
-		//board.setFileName(file.getOriginalFilename());
+		// board.setFileName(file.getOriginalFilename());
 		Board updateBoard = boardRepository.findOne(id);
 		updateBoard.setTitle(board.getTitle());
 		updateBoard.setContents(board.getContents());
 		updateBoard.setFileName(file.getOriginalFilename());
 		boardRepository.save(updateBoard);
-		
+
 		return "redirect:/board/list";
 	}
-	
+
 }
